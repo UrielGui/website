112ffe07e49e5f93ff203f297009d8e4e8e15368
Uriel Guimarães <57813312+UrielDevPro@users.noreply.github.com>
2021-03-12 04:49:30 -0300

    Configurei os links das redes sociais

--- components/social-media.jsx
+++ components/social-media.jsx
@@ -8,28 +8,34 @@ export default function SocialMedia() {
   return (
     <div className="social-media flex justify-center mt-7">
       <div className="button-social-media">
-        <div className="icon p-4">
-          <i className="flex justify-center items-center">
-            <FaFacebookF />
-          </i>
-        </div>
-        <span>Facebook</span>
+        <a href="https://www.facebook.com/uriel.guimaraes.14/" target="_blank">
+          <div className="icon p-4">
+            <i className="flex justify-center items-center">
+              <FaFacebookF />
+            </i>
+          </div>
+          <span>Facebook</span>
+        </a>
       </div>
       <div className="button-social-media">
-        <div className="icon p-4">
-          <i className="flex justify-center items-center">
-            <FaInstagram />
-          </i>
-        </div>
-        <span>Instagram</span>
+        <a href="https://www.instagram.com/urieleffe/" target="_blank">
+          <div className="icon p-4">
+            <i className="flex justify-center items-center">
+              <FaInstagram />
+            </i>
+          </div>
+          <span>Instagram</span>
+        </a>
       </div>
       <div className="button-social-media">
-        <div className="icon p-4">
-          <i className="flex justify-center items-center">
-            <FaGithub />
-          </i>
-        </div>
-        <span>GitHub</span>
+        <a href="https://github.com/UrielGui/" target="_blank">
+          <div className="icon p-4">
+            <i className="flex justify-center items-center">
+              <FaGithub />
+            </i>
+          </div>
+          <span>GitHub</span>
+        </a>
       </div>
       <div className="button-social-media">
         <Link to="contato">
