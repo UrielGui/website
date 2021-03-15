f7f3f9ae38fb47ed403e69f786b265a3f79a6b58
Uriel Guimarães <57813312+UrielDevPro@users.noreply.github.com>
2021-03-15 17:05:08 -0300

    Bugs resolvidos

--- pages/header/menu/menu-responsivo.jsx
+++ pages/header/menu/menu-responsivo.jsx
@@ -1,24 +1,21 @@
 import { Link } from "react-scroll";
 import { FaGithub } from 'react-icons/fa';
-import FadeIn from 'react-fade-in';
 
 export default function MenuResponsivo() {
     return (
-        <FadeIn>
-            <div className="flex flex-col p-8 menu-mobile">
-                <Link to="sobre" className="my-1.5 cursor-pointer text-gray-300 hover:text-uriel-blue-light">
-                    Sobre
+        <div className="flex flex-col p-8 menu-mobile">
+            <Link to="sobre" className="my-1.5 cursor-pointer text-gray-300 hover:text-uriel-blue-light">
+                Sobre
             </Link>
-                <Link to="habilidades" className="my-1.5 cursor-pointer text-gray-300 hover:text-uriel-blue-light">
-                    Habilidades
+            <Link to="habilidades" className="my-1.5 cursor-pointer text-gray-300 hover:text-uriel-blue-light">
+                Habilidades
             </Link>
-                <Link to="contato" className="my-1.5 cursor-pointer text-gray-300 hover:text-uriel-blue-light">
-                    Contato
+            <Link to="contato" className="my-1.5 cursor-pointer text-gray-300 hover:text-uriel-blue-light">
+                Contato
             </Link>
-                <a href="https://github.com/UrielGui" target="_blank" className="cursor-pointer text-purple-500 hover:text-uriel-blue-light">
-                    <FaGithub className="inline mb-1 mr-px" /> GitHub
+            <a href="https://github.com/UrielGui" target="_blank" className="cursor-pointer text-purple-500 hover:text-uriel-blue-light">
+                <FaGithub className="inline mb-1 mr-px" /> GitHub
             </a><br />
-            </div>
-        </FadeIn>
+        </div>
     )
 }
