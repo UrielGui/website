0387da3020f6311fe0ead206fa7cf544691273d9
Uriel Guimarães <urieligara@hotmail.com>
2021-06-10 23:47:05 -0300

    bugs fixed

--- pages/sobre/index.jsx
+++ pages/sobre/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import LazyLoad from 'react-lazyload';
 import TextPage from '../../components/text-page'
 import Apresentacao from './apresentacao'
@@ -22,29 +21,4 @@ export default function Sobre() {
             </section>
         </>
     )
-=======
-import LazyLoad from 'react-lazyload';
-import TextPage from '../../components/text-page'
-import Apresentacao from './apresentacao'
-
-export default function Sobre() {
-    return (
-        <>
-            <section id="sobre" className="h-full">
-                <div className="min-h-screen overflow-auto bg-sobre-background bg-no-repeat bg-cover bg-center">
-                    <LazyLoad>
-                        <div className="pl-10 pt-4 md:min-h-screen">
-                            <TextPage titulo="Descubra um pouco sobre mim" />
-                            <div className="flex flex-col md:flex-row md:space-x-12">
-                                <div className="bg-sobre-foto bg-no-repeat bg-center bg-contain w-full md:w-1/3 md:mt-10 rounded-3xl s-bg-1 s-bg-2">
-                                </div>
-                                <Apresentacao />
-                            </div>
-                        </div>
-                    </LazyLoad>
-                </div>
-            </section>
-        </>
-    )
->>>>>>> 4f6afa82918f9f09bae9fe526fd87c8f524e62f4
 }
