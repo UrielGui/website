0fce8cb4733515f8a026daa67b3af4ccf8513ce1
Uriel Guimarães <urieligara@hotmail.com>
2021-06-28 20:46:59 -0300

    portfolio added in menu & github removed

--- pages/header/menu/menu-desktop.jsx
+++ pages/header/menu/menu-desktop.jsx
@@ -1,7 +1,5 @@
 import { Link } from "react-scroll";
 
-import { FaGithub } from 'react-icons/fa';
-
 export default function MenuDesktop() {
     return (
         <>
@@ -11,12 +9,12 @@ export default function MenuDesktop() {
             <Link to="habilidades" className="cursor-pointer text-gray-300 hover:text-uriel-blue-light">
                 Habilidades
             </Link>
+            <Link to="habilidades" className="cursor-pointer text-gray-300 hover:text-uriel-blue-light">
+                Portfólio
+            </Link>
             <Link to="contato" className="cursor-pointer text-gray-300 hover:text-uriel-blue-light">
                 Contato
             </Link>
-            <a href="https://github.com/UrielGui" target="_blank" className="cursor-pointer text-purple-500 hover:text-uriel-blue-light">
-                <FaGithub className="inline mb-1 mr-px" /> GitHub
-            </a>
         </>
     )
 
