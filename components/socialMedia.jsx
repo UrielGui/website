dce35071464afb6b0b34bcb44a875fe6800ce925
Uriel Guimarães <urieligara@hotmail.com>
2021-06-30 05:24:25 -0300

    version updated

---
+++ components/socialMedia.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-scroll";
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaDribbble } from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai';
+
+export default function SocialMedia() {
+  return (
+    <div className="flex sm:mt-7 mt-2 flex-wrap">
+        <a href="https://www.linkedin.com/in/urielgui" target="_blank" className="bg-blue-600 text-white sm:text-2xl p-4 rounded-xl cursor-pointer hover:bg-white hover:text-black mr-4">
+        <FaLinkedinIn />
+      </a>
+      <a href="https://github.com/UrielGui" target="_blank" className="bg-purple-600 text-white sm:text-2xl p-4 rounded-xl cursor-pointer hover:bg-white hover:text-black mr-4">
+        <FaGithub />
+      </a>
+      <a href="https://dribbble.com/UrielGui" target="_blank" className="bg-pink-600 text-white sm:text-2xl p-4 rounded-xl cursor-pointer hover:bg-white hover:text-black mr-4">
+        <FaDribbble />
+      </a>
+      <Link to="contact" smooth={true} className="bg-green-600 text-white sm:text-2xl p-4 rounded-xl cursor-pointer hover:bg-white hover:text-black mr-4">
+        <AiOutlineMail />
+      </Link>
+    </div>
+  )
+}
