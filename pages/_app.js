af3e0d41b659b54996875947cd79dcb3b2a6f5a8
Uriel Guimarães <urieligara@hotmail.com>
2021-07-02 21:42:37 -0300

    bug fixed at scroll bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-//import '../styles/tailwind.css';
+// import '../styles/tailwind.css';
 import '../public/css/main.css';
 
 function UrielPortfolio({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 };
 
 export default UrielPortfolio;
