3abee316cf0b7e774877b25054f5f2c3846eb718
Uriel Guimarães <urieligara@hotmail.com>
2021-07-02 23:12:40 -0300

    portfolio animation updated

--- pages/portfolio/portfolioGallery.jsx
+++ pages/portfolio/portfolioGallery.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import Data from '../../components/data';
+import PortfolioAnimation from '../../components/animations/portfolioAnimation';
 
 export default function PortfolioGallery() {
-    let [categoryProduct, setCategoryProduct] = useState('all');
+    let [productCategory, setProductCategory] = useState('all');
     let [active, setActive] = useState('m-1');
+
+    function onClickMenu(category, active) {
+        setProductCategory(category), setActive(active);
+    }
 
     const menuItems = () => (
         <React.Fragment>
@@ -13,12 +18,7 @@ export default function PortfolioGallery() {
                         ? 'p-2 px-3.5 mx-6 portfolio-active-menu'
                         : 'p-2 mx-6 cursor-pointer'
                 }
-                onClick={() =>
-                    setCategoryProduct(
-                        (categoryProduct = 'all'),
-                        setActive((active = 'm-1'))
-                    )
-                }
+                onClick={() => onClickMenu('all', 'm-1')}
             >
                 Todos
             </span>
@@ -29,12 +29,7 @@ export default function PortfolioGallery() {
                         ? 'p-2 px-3.5 mx-6 portfolio-active-menu'
                         : 'p-2 mx-6 cursor-pointer'
                 }
-                onClick={() =>
-                    setCategoryProduct(
-                        (categoryProduct = 'web'),
-                        setActive((active = 'm-2'))
-                    )
-                }
+                onClick={() => onClickMenu('web', 'm-2')}
             >
                 Web
             </span>
@@ -45,12 +40,7 @@ export default function PortfolioGallery() {
                         ? 'p-2 px-3.5 mx-6 portfolio-active-menu'
                         : 'p-2 mx-6 cursor-pointer'
                 }
-                onClick={() =>
-                    setCategoryProduct(
-                        (categoryProduct = 'app'),
-                        setActive((active = 'm-3'))
-                    )
-                }
+                onClick={() => onClickMenu('app', 'm-3')}
             >
                 App
             </span>
@@ -61,12 +51,7 @@ export default function PortfolioGallery() {
                         ? 'p-2 px-3.5 mx-6 portfolio-active-menu'
                         : 'p-2 mx-6 cursor-pointer'
                 }
-                onClick={() =>
-                    setCategoryProduct(
-                        (categoryProduct = 'ui design'),
-                        setActive((active = 'm-4'))
-                    )
-                }
+                onClick={() => onClickMenu('ui design', 'm-4')}
             >
                 Ui Design
             </span>
@@ -77,10 +62,10 @@ export default function PortfolioGallery() {
         <>
             {Data.products
                 .filter((val) => {
-                    if (categoryProduct === 'all') {
-                        return val.category;
+                    if (productCategory === 'all') {
+                        return val;
                     } else {
-                        return val.category === categoryProduct;
+                        return val.category === productCategory;
                     }
                 })
                 .sort((a, b) => (a.description > b.description ? 1 : -1))
@@ -91,33 +76,38 @@ export default function PortfolioGallery() {
                             key={product.id}
                             className='lg:w-3/7 mt-10 three mb-8'
                         >
-                            <div className='flex w-full p-6 bg-green-800 text-white sm:flex-row flex-col-reverse'>
-                                <div className='sm:w-1/2 w-full'>
-                                    <div className='pr-4'>
-                                        <div className='text-2xl'>
-                                            {product.title}
+                            <PortfolioAnimation>
+                                <div className='flex w-full p-6 bg-green-800 text-white sm:flex-row flex-col-reverse'>
+                                    <div className='sm:w-1/2 w-full'>
+                                        <div className='pr-4'>
+                                            <div className='text-2xl'>
+                                                {product.title}
+                                            </div>
+                                            <div className='mt-5 text-xs '>
+                                                {product.description}
+                                            </div>
+                                            <div className='mt-5 text-sm mb-5'>
+                                                {product.technologies}
+                                            </div>
+                                            <a
+                                                href={product.link}
+                                                target='_blank'
+                                            >
+                                                <button className='uppercase bg-main-color p-2.5 rounded-lg px-6 hover:bg-white hover:text-main-color sm:m-0 m-auto flex'>
+                                                    {product.more}
+                                                </button>
+                                            </a>
                                         </div>
-                                        <div className='mt-5 text-xs '>
-                                            {product.description}
-                                        </div>
-                                        <div className='mt-5 text-sm mb-5'>
-                                            {product.technologies}
-                                        </div>
-                                        <a href={product.link} target='_blank'>
-                                            <button className='uppercase bg-main-color p-2.5 rounded-lg px-6 hover:bg-white hover:text-main-color sm:m-0 m-auto flex'>
-                                                {product.more}
-                                            </button>
-                                        </a>
+                                    </div>
+                                    <div className='sm:w-1/2 w-full'>
+                                        <img
+                                            className='sm:h-56 sm:w-72 sm:pb-0 pb-4'
+                                            alt={product.name}
+                                            src={product.image}
+                                        />
                                     </div>
                                 </div>
-                                <div className='sm:w-1/2 w-full'>
-                                    <img
-                                        className='sm:h-56 sm:w-72 sm:pb-0 pb-4'
-                                        alt={product.name}
-                                        src={product.image}
-                                    />
-                                </div>
-                            </div>
+                            </PortfolioAnimation>
                         </div>
                     );
                 })}
